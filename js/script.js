4dd9021db5b4acaa733896148f6cfb6e35b431d0
Leo Schultz <leo@cuore.io>
2014-09-08 16:28:36 -0700

    Site Update

--- js/script.js
+++ js/script.js
@@ -12,6 +12,25 @@ $(window).load(function() {
 
 });
 
+$( document ).ready(function() {
+    $(".tile").height($("#tile1").width());
+    $(".carousel").height($("#tile1").width());
+     $(".item").height($("#tile1").width());
+     
+    $(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+  this.resizeTO = setTimeout(function() {
+    $(this).trigger('resizeEnd');
+  }, 10);
+    });
+    
+    $(window).bind('resizeEnd', function() {
+      $(".tile").height($("#tile1").width());
+        $(".carousel").height($("#tile1").width());
+        $(".item").height($("#tile1").width());
+    });
+
+});
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
